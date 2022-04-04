b12805193040064c48b3398c417d8a818f1a0a53
sashtje <sashtje.lefevre@gmail.com>
2022-04-04 20:37:46 +0300

    feat: add test for NotificationWindow component

--- react-components/src/components/DateField/DateField.test.tsx
+++ react-components/src/components/DateField/DateField.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { screen, render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+
 import DateField from './DateField';
 
 test('Check handleChange of DateField component with error', () => {
