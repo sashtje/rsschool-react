2b5d5a1c66e6cec56b52c76f122b02a37f3eca3d
sashtje <sashtje.lefevre@gmail.com>
2022-04-04 17:00:27 +0300

    feat: add Checkbox render test

---
+++ react-components/src/components/Checkbox/Checkbox.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import Checkbox from './Checkbox';
+
+test('Check render of checkbox component', () => {
+  const label = 'Test';
+  const checkRef: React.RefObject<HTMLInputElement> = React.createRef();
+  render(<Checkbox label={label} checkboxRef={checkRef} />);
+
+  const checkbox = screen.getByRole('checkbox', {
+    name: label,
+  });
+
+  expect(checkbox).toBeInTheDocument();
+});
--- react-components/src/pages/FormPage/FormPage.test.tsx
+++ react-components/src/pages/FormPage/FormPage.test.tsx
@@ -8,22 +8,4 @@ import { renderWithRouter } from '../../testAPI/testapi';
 
 test('Navigation Test for Router', () => {
   renderWithRouter(<FormPage />);
-  /* const linkHome = screen.getByText(/Home/i);
-  const linkAbout = screen.getByText(/About us/i);
-
-  let main = screen.getByRole('main');
-  expect(main).toBeInTheDocument();
-  expect(main).toHaveClass('home');
-
-  userEvent.click(linkAbout);
-
-  main = screen.getByRole('main');
-  expect(main).not.toHaveClass('home');
-  expect(main).toHaveClass('about');
-
-  userEvent.click(linkHome);
-
-  main = screen.getByRole('main');
-  expect(main).toHaveClass('home');
-  expect(main).not.toHaveClass('about'); */
 });
