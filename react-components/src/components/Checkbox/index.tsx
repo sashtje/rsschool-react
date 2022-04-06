5fd146bc2f30b14061b929c5833a365c3a9c23ad
sashtje <sashtje.lefevre@gmail.com>
2022-04-06 17:01:13 +0300

    refactor: add destructuring to Checkbox props

--- react-components/src/components/Checkbox/index.tsx
+++ react-components/src/components/Checkbox/index.tsx
@@ -6,11 +6,13 @@ import IProps from './types';
 
 class Checkbox extends Component<IProps> {
   render() {
+    const { checkboxRef, label } = this.props;
+
     return (
       <label className="checkbox">
-        <input ref={this.props.checkboxRef} className="checkbox__input" type="checkbox" />
+        <input ref={checkboxRef} className="checkbox__input" type="checkbox" />
         <div className="checkbox__checkbox"></div>
-        <span className="checkbox__label">{this.props.label}</span>
+        <span className="checkbox__label">{label}</span>
       </label>
     );
   }
