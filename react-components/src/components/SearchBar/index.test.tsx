1ccba2e2f9375d3079c91b18796f8a147e094717
sashtje <sashtje.lefevre@gmail.com>
2022-04-11 18:09:31 +0300

    feat: change test for SearchBar

--- react-components/src/components/SearchBar/index.test.tsx
+++ react-components/src/components/SearchBar/index.test.tsx
@@ -5,18 +5,56 @@ import App from '../../App';
 
 import { renderWithRouter } from '../../testAPI';
 
-test('Save to the local storage', () => {
+interface IStorage {
+  [key: string]: string;
+}
+
+const localStorageMock = (() => {
+  let store: IStorage = { searchbar: 'happiness' };
+
+  return {
+    getItem(key: string) {
+      return store[key];
+    },
+    setItem(key: string, value: string) {
+      store[key] = value.toString();
+    },
+    clear() {
+      store = {};
+    },
+    removeItem(key: string) {
+      delete store[key];
+    },
+  };
+})();
+
+Object.defineProperty(window, 'localStorage', {
+  value: localStorageMock,
+});
+
+test('test with mock local storage with initial value', () => {
+  const getItem = jest.spyOn(window.localStorage, 'getItem');
+  const setItem = jest.spyOn(window.localStorage, 'setItem');
   renderWithRouter(<App />);
+
   const linkHome = screen.getByText(/Home/i);
   const linkAbout = screen.getByText(/About us/i);
   const input = screen.getByRole('searchbox') as HTMLInputElement;
-  const value = 'Turkey';
+  expect(input).toHaveValue('happiness');
+
+  expect(getItem).toHaveBeenCalledTimes(2);
+
+  const value = 'love';
 
   input.value = '';
 
   userEvent.type(input, value);
   expect(input).toHaveValue(value);
   userEvent.click(linkAbout);
+
+  expect(setItem).toHaveBeenCalledTimes(1);
+
   userEvent.click(linkHome);
   expect(input).toHaveValue(value);
+  expect(getItem).toHaveBeenCalledTimes(4);
 });
