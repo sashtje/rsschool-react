d90f326d007669c807fe2409cc8a29cf47ea4549
sashtje <sashtje.lefevre@gmail.com>
2022-04-11 21:56:55 +0300

    fix: fix tests for CardList

--- react-components/src/components/CardList/index.test.tsx
+++ react-components/src/components/CardList/index.test.tsx
@@ -1,29 +1,42 @@
 import { render, screen } from '@testing-library/react';
 
-import App from '../../App';
 import CardList from '.';
 
-import { renderWithRouter } from '../../testAPI';
+import { IData } from '../../pages/Main/types';
 
-test('CardList render test', () => {
-  /* renderWithRouter(<App />);
-  const input = screen.getByRole('searchbox') as HTMLInputElement;
-  const cards = screen.getAllByTestId('card');
-  if (input.value === '') {
-    expect(cards.length).toBe(data.length);
-  } */
-});
+const data: IData[] = [
+  {
+    id: '123',
+    title: 'Test',
+    ownername: 'Owner',
+    views: '124',
+    url: '',
+  },
+  {
+    id: '345',
+    title: 'Art',
+    ownername: 'Yellow',
+    views: '500',
+    url: '',
+  },
+];
 
 test('Pass class from props', () => {
-  /* render(<CardList className="home__cards" search={'123'} />);
+  render(<CardList className="home__cards" data={data} />);
   const cardsDiv = screen.getByTestId('cards');
   expect(cardsDiv).toHaveClass('home__cards');
-  expect(cardsDiv).toHaveClass('cards'); */
+  expect(cardsDiv).toHaveClass('cards');
+
+  const card = screen.getAllByTestId('card');
+  expect(card.length).toBe(2);
 });
 
 test('CardList without props class', () => {
-  /* render(<CardList search={'123'} />);
+  render(<CardList data={data} />);
   const cardsDiv = screen.getByTestId('cards');
   expect(cardsDiv).not.toHaveClass('home__cards');
-  expect(cardsDiv).toHaveClass('cards'); */
+  expect(cardsDiv).toHaveClass('cards');
+
+  const card = screen.getAllByTestId('card');
+  expect(card.length).toBe(2);
 });
