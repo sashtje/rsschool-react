8a20a64ae4f48a6f94d80e2cc4bd6de9b3447688
sashtje <sashtje.lefevre@gmail.com>
2022-04-15 14:23:17 +0300

    feat: move App.tsx from class component to function component

--- react-components/src/App.tsx
+++ react-components/src/App.tsx
@@ -1,32 +1,26 @@
-import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 import Main from './pages/Main';
-import About from './pages/About';
-import Page404 from './pages/Page404';
-import FormPage from './pages/FormPage';
 import Layout from './components/Layout';
 
 import './App.scss';
 
 import { routes } from './router';
 
-class App extends React.Component {
-  render() {
-    return (
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Main />} />
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Main />} />
 
-          {routes.map((route, index) => (
-            <Route key={index} path={route.path} element={<route.element />} />
-          ))}
+        {routes.map((route, index) => (
+          <Route key={index} path={route.path} element={<route.element />} />
+        ))}
 
-          <Route path="*" element={<Navigate to="/404" replace />} />
-        </Route>
-      </Routes>
-    );
-  }
-}
+        <Route path="*" element={<Navigate to="/404" replace />} />
+      </Route>
+    </Routes>
+  );
+};
 
 export default App;
