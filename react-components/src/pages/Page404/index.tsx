0384169dbb2ceafcec40fa273c16fdcf691874a1
sashtje <sashtje.lefevre@gmail.com>
2022-04-15 15:26:30 +0300

    feat: move Page404 to functional component

--- react-components/src/pages/Page404/index.tsx
+++ react-components/src/pages/Page404/index.tsx
@@ -1,11 +1,7 @@
-import { Component } from 'react';
-
 import './styles.scss';
 
-class Page404 extends Component {
-  render() {
-    return <div className="page404">Unfortunately page not found</div>;
-  }
-}
+const Page404 = () => {
+  return <div className="page404">Unfortunately page not found</div>;
+};
 
 export default Page404;
