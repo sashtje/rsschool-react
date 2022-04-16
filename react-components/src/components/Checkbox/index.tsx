b7fdc085fb90c64cb412c6881ebe51a7c6f888ab
sashtje <sashtje.lefevre@gmail.com>
2022-04-16 21:15:18 +0300

    feat: move Checkbox to functional

--- react-components/src/components/Checkbox/index.tsx
+++ react-components/src/components/Checkbox/index.tsx
@@ -1,21 +1,15 @@
-import { Component } from 'react';
-
 import './styles.scss';
 
 import IProps from './types';
 
-class Checkbox extends Component<IProps> {
-  render() {
-    const { checkboxRef, label } = this.props;
-
-    return (
-      <label className="checkbox">
-        <input ref={checkboxRef} className="checkbox__input" type="checkbox" />
-        <div className="checkbox__checkbox"></div>
-        <span className="checkbox__label">{label}</span>
-      </label>
-    );
-  }
-}
+const Checkbox = ({ checkboxRef, label }: IProps) => {
+  return (
+    <label className="checkbox">
+      <input ref={checkboxRef} className="checkbox__input" type="checkbox" />
+      <div className="checkbox__checkbox"></div>
+      <span className="checkbox__label">{label}</span>
+    </label>
+  );
+};
 
 export default Checkbox;
