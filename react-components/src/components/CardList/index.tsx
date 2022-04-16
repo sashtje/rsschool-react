2293130060dca4a6cfaff56292b179ec2dc3a349
sashtje <sashtje.lefevre@gmail.com>
2022-04-16 15:10:50 +0300

    feat: move CardList to functional component

--- react-components/src/components/CardList/index.tsx
+++ react-components/src/components/CardList/index.tsx
@@ -1,23 +1,17 @@
-import { Component } from 'react';
-
 import CardItem from '../CardItem';
 
 import './styles.scss';
 
 import IProps from './types';
 
-class CardList extends Component<IProps> {
-  render() {
-    const { className, data } = this.props;
-
-    return (
-      <div className={className ? className + ' cards' : 'cards'} data-testid="cards">
-        {data.map((item) => (
-          <CardItem card={item} key={item.id} />
-        ))}
-      </div>
-    );
-  }
-}
+const CardList = ({ className, data }: IProps) => {
+  return (
+    <div className={className ? className + ' cards' : 'cards'} data-testid="cards">
+      {data.map((item) => (
+        <CardItem card={item} key={item.id} />
+      ))}
+    </div>
+  );
+};
 
 export default CardList;
