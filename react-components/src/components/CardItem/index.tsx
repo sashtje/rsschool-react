feb900587ea7e756fe44ce5a0f229d2083875c54
sashtje <sashtje.lefevre@gmail.com>
2022-04-16 20:46:17 +0300

    feat: move CardItem to functional component

--- react-components/src/components/CardItem/index.tsx
+++ react-components/src/components/CardItem/index.tsx
@@ -1,52 +1,45 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { AiOutlineEye } from 'react-icons/ai';
 
 import ModalWindow from '../ModalWindow';
 
 import './styles.scss';
 
-import { IProps, IState } from './types';
+import { IProps } from './types';
 
-class CardItem extends Component<IProps, IState> {
-  state = { isModalWindowShown: false };
+const CardItem = (props: IProps) => {
+  const [isModalWindowShown, setIsModalWindowShown] = useState(false);
 
-  handleClick = () => {
-    this.setState((state) => ({
-      isModalWindowShown: !state.isModalWindowShown,
-    }));
+  const handleClick = () => {
+    setIsModalWindowShown(!isModalWindowShown);
   };
 
-  render() {
-    const { title, ownername, views, url } = this.props.card;
-    const { isModalWindowShown } = this.state;
+  const { title, ownername, views, url } = props.card;
 
-    return (
-      <section className="card" data-testid="card" onClick={this.handleClick}>
-        <div
-          className="card__photo"
-          data-testid="card-photo"
-          style={{ backgroundImage: `url(${url})` }}
-        ></div>
-        <h2 className="card__country" data-testid="card-title">
-          {title}
-        </h2>
-        <div className="card__author">
-          by{' '}
-          <span className="card__author-name" data-testid="card-author">
-            {ownername}
-          </span>
-        </div>
-        <div className="card__feedback" data-testid="card-views">
-          <AiOutlineEye />
-          {views}
-        </div>
+  return (
+    <section className="card" data-testid="card" onClick={handleClick}>
+      <div
+        className="card__photo"
+        data-testid="card-photo"
+        style={{ backgroundImage: `url(${url})` }}
+      ></div>
+      <h2 className="card__country" data-testid="card-title">
+        {title}
+      </h2>
+      <div className="card__author">
+        by{' '}
+        <span className="card__author-name" data-testid="card-author">
+          {ownername}
+        </span>
+      </div>
+      <div className="card__feedback" data-testid="card-views">
+        <AiOutlineEye />
+        {views}
+      </div>
 
-        {isModalWindowShown && (
-          <ModalWindow closeWindow={this.handleClick} card={this.props.card} />
-        )}
-      </section>
-    );
-  }
-}
+      {isModalWindowShown && <ModalWindow closeWindow={handleClick} card={props.card} />}
+    </section>
+  );
+};
 
 export default CardItem;
