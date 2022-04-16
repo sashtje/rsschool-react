171da1c013c830a6e89943435db101829a6de243
sashtje <sashtje.lefevre@gmail.com>
2022-04-16 21:18:08 +0300

    feat: move Header to functional

--- react-components/src/components/Header/index.tsx
+++ react-components/src/components/Header/index.tsx
@@ -1,27 +1,24 @@
-import { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
 import './styles.scss';
 
-class Header extends Component {
-  setActive = ({ isActive }: { isActive: boolean }) =>
+const Header = () => {
+  const setActive = ({ isActive }: { isActive: boolean }) =>
     isActive ? 'header__link header__link_is_chosen' : 'header__link';
 
-  render() {
-    return (
-      <header className="header">
-        <NavLink className={this.setActive} to="/">
-          Home
-        </NavLink>
-        <NavLink className={this.setActive} to="/form">
-          Form
-        </NavLink>
-        <NavLink className={this.setActive} to="/about">
-          About us
-        </NavLink>
-      </header>
-    );
-  }
-}
+  return (
+    <header className="header">
+      <NavLink className={setActive} to="/">
+        Home
+      </NavLink>
+      <NavLink className={setActive} to="/form">
+        Form
+      </NavLink>
+      <NavLink className={setActive} to="/about">
+        About us
+      </NavLink>
+    </header>
+  );
+};
 
 export default Header;
