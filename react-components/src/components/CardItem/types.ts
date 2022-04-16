8b25d96bf7499864652c2185e42761b44fe4c155
sashtje <sashtje.lefevre@gmail.com>
2022-04-16 21:05:22 +0300

    feat: move ModalWindow to functional component

--- react-components/src/components/CardItem/types.ts
+++ react-components/src/components/CardItem/types.ts
@@ -2,7 +2,3 @@ import { IData } from '../../pages/Main/types';
 export interface IProps {
   card: IData;
 }
-
-export interface IState {
-  isModalWindowShown: boolean;
-}
--- react-components/src/components/ModalWindow/index.tsx
+++ react-components/src/components/ModalWindow/index.tsx
@@ -1,23 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { AiOutlineCloseSquare, AiOutlineEye } from 'react-icons/ai';
 
 import './styles.scss';
 
 import { IProps } from './types';
 
-class ModalWindow extends Component<IProps> {
-  handleCloseWindow = (e: React.MouseEvent) => {
-    const { closeWindow } = this.props;
-
+const ModalWindow = ({ closeWindow, card }: IProps) => {
+  const handleCloseWindow = (e: React.MouseEvent) => {
     e.stopPropagation();
     closeWindow();
   };
 
-  stopClick = (e: React.MouseEvent) => {
+  const stopClick = (e: React.MouseEvent) => {
     e.stopPropagation();
   };
 
-  getLocalDateTaken = (date: string) => {
+  const getLocalDateTaken = (date: string) => {
     let dateLocal;
 
     dateLocal = new Date(date).toLocaleString();
@@ -29,78 +27,72 @@ class ModalWindow extends Component<IProps> {
     return dateLocal;
   };
 
-  render() {
-    const {
-      title,
-      description,
-      ownername,
-      datetaken,
-      dateupload,
-      lastupdate,
-      views,
-      tags,
-      latitude,
-      longitude,
-      url,
-    } = this.props.card;
+  const {
+    title,
+    description,
+    ownername,
+    datetaken,
+    dateupload,
+    lastupdate,
+    views,
+    tags,
+    latitude,
+    longitude,
+    url,
+  } = card;
 
-    return (
-      <div className="modalwindow" onClick={this.handleCloseWindow} data-testid="modal-window">
-        <div
-          className="modalwindow__content"
-          onClick={this.stopClick}
-          data-testid="modal-window-content"
-        >
-          <span className="modalwindow__close-icon" onClick={this.handleCloseWindow}>
-            <AiOutlineCloseSquare />
-          </span>
+  return (
+    <div className="modalwindow" onClick={handleCloseWindow} data-testid="modal-window">
+      <div className="modalwindow__content" onClick={stopClick} data-testid="modal-window-content">
+        <span className="modalwindow__close-icon" onClick={handleCloseWindow}>
+          <AiOutlineCloseSquare />
+        </span>
 
-          <div className="modalwindow__photo">
-            <img src={url} alt={`flickr photo ${tags}`} />
+        <div className="modalwindow__photo">
+          <img src={url} alt={`flickr photo ${tags}`} />
+        </div>
+
+        <h2 className="modalwindow__title">{title}</h2>
+
+        <div className="modalwindow__author">
+          by <span className="modalwindow__author-name">{ownername}</span>
+        </div>
+
+        <div className="modalwindow__feedback">
+          <AiOutlineEye />
+          {views}
+        </div>
+
+        {description?._content && (
+          <div className="modalwindow__description">
+            <b>Description:</b> {description?._content}
           </div>
+        )}
 
-          <h2 className="modalwindow__title">{title}</h2>
-
-          <div className="modalwindow__author">
-            by <span className="modalwindow__author-name">{ownername}</span>
-          </div>
-
-          <div className="modalwindow__feedback">
-            <AiOutlineEye />
-            {views}
-          </div>
-
-          {description?._content && (
-            <div className="modalwindow__description">
-              <b>Description:</b> {description?._content}
-            </div>
-          )}
-
-          {datetaken && (
-            <div className="modalwindow__block">
-              <b>Date taken:</b> {this.getLocalDateTaken(datetaken)}
-            </div>
-          )}
-
-          {dateupload && (
-            <div className="modalwindow__block">
-              <b>Date upload:</b> {new Date(+dateupload).toLocaleString()}
-            </div>
-          )}
-
-          {lastupdate && (
-            <div className="modalwindow__block">
-              <b>Last update:</b> {new Date(+lastupdate).toLocaleString()}
-            </div>
-          )}
-
+        {datetaken && (
           <div className="modalwindow__block">
-            <b>Geo:</b> <i>lat</i> {latitude}, <i>lon</i> {longitude}
+            <b>Date taken:</b> {getLocalDateTaken(datetaken)}
           </div>
+        )}
+
+        {dateupload && (
+          <div className="modalwindow__block">
+            <b>Date upload:</b> {new Date(+dateupload).toLocaleString()}
+          </div>
+        )}
+
+        {lastupdate && (
+          <div className="modalwindow__block">
+            <b>Last update:</b> {new Date(+lastupdate).toLocaleString()}
+          </div>
+        )}
+
+        <div className="modalwindow__block">
+          <b>Geo:</b> <i>lat</i> {latitude}, <i>lon</i> {longitude}
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default ModalWindow;
