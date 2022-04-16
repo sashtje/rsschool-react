06afc5c7208783ca2b6a8c32af84cee33e198910
sashtje <sashtje.lefevre@gmail.com>
2022-04-16 21:16:37 +0300

    feat: move DateField to functional

--- react-components/src/components/DateField/index.tsx
+++ react-components/src/components/DateField/index.tsx
@@ -1,31 +1,25 @@
-import { Component } from 'react';
-
 import Label from '../Label';
 
 import './styles.scss';
 
 import IProps from './types';
 
-class DateField extends Component<IProps> {
-  render() {
-    const { label, dateRef, textError, name, handleChangeInput } = this.props;
+const DateField = ({ label, dateRef, textError, name, handleChangeInput }: IProps) => {
+  return (
+    <div className="datefield">
+      <Label>
+        {label}
+        <input
+          ref={dateRef}
+          className="datefield__input"
+          type="date"
+          onChange={() => handleChangeInput(`${name}Error`, textError)}
+        />
+      </Label>
 
-    return (
-      <div className="datefield">
-        <Label>
-          {label}
-          <input
-            ref={dateRef}
-            className="datefield__input"
-            type="date"
-            onChange={() => handleChangeInput(`${name}Error`, textError)}
-          />
-        </Label>
-
-        {textError !== '' && <div className="datefield__validation">{textError}</div>}
-      </div>
-    );
-  }
-}
+      {textError !== '' && <div className="datefield__validation">{textError}</div>}
+    </div>
+  );
+};
 
 export default DateField;
