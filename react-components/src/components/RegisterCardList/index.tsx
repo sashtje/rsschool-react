75ce64091d702f36d7b619a39c61c84ce60477eb
sashtje <sashtje.lefevre@gmail.com>
2022-04-17 17:03:22 +0300

    feat: move RegisterCardList to functional component

--- react-components/src/components/RegisterCardList/index.tsx
+++ react-components/src/components/RegisterCardList/index.tsx
@@ -1,29 +1,21 @@
-import { Component } from 'react';
-
 import RegisterCardItem from '../RegisterCardItem';
 
 import './styles.scss';
 
 import { IProps } from './types';
 
-class RegisterCardList extends Component<IProps> {
-  returnClass = () => {
-    const { className } = this.props;
-
+const RegisterCardList = ({ className, cards }: IProps) => {
+  const returnClass = () => {
     return className ? `registercards ${className}` : 'registercards';
   };
 
-  render() {
-    const { cards } = this.props;
-
-    return (
-      <div data-testid="regcards-container" className={this.returnClass()}>
-        {cards.map((card) => (
-          <RegisterCardItem card={card} key={card.id} />
-        ))}
-      </div>
-    );
-  }
-}
+  return (
+    <div data-testid="regcards-container" className={returnClass()}>
+      {cards.map((card) => (
+        <RegisterCardItem card={card} key={card.id} />
+      ))}
+    </div>
+  );
+};
 
 export default RegisterCardList;
