e894c169ea0e5e7170b5d1ba9d9a3877bca50f78
sashtje <sashtje.lefevre@gmail.com>
2022-04-17 17:13:05 +0300

    feat: move Switcher to functional component

--- react-components/src/components/Switcher/index.tsx
+++ react-components/src/components/Switcher/index.tsx
@@ -1,27 +1,21 @@
-import { Component } from 'react';
-
 import Label from '../Label';
 
 import './styles.scss';
 
 import { IProps } from './types';
 
-class Switcher extends Component<IProps> {
-  render() {
-    const { label, switcherRef } = this.props;
-
-    return (
-      <Label className="label_is_inline-flex">
-        {label}
-        <label className="switcher">
-          <input ref={switcherRef} className="switcher__input" type="checkbox" />
-          <span className="switcher__track">
-            <span className="switcher__slider"></span>
-          </span>
-        </label>
-      </Label>
-    );
-  }
-}
+const Switcher = ({ label, switcherRef }: IProps) => {
+  return (
+    <Label className="label_is_inline-flex">
+      {label}
+      <label className="switcher">
+        <input ref={switcherRef} className="switcher__input" type="checkbox" />
+        <span className="switcher__track">
+          <span className="switcher__slider"></span>
+        </span>
+      </label>
+    </Label>
+  );
+};
 
 export default Switcher;
