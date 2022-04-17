934059226223b7761a9d2870e181cc1d66208606
sashtje <sashtje.lefevre@gmail.com>
2022-04-17 17:19:32 +0300

    feat: move UploadPhoto to functional component

--- react-components/src/components/UploadPhoto/index.tsx
+++ react-components/src/components/UploadPhoto/index.tsx
@@ -1,27 +1,21 @@
-import { Component } from 'react';
-
 import './styles.scss';
 
 import { IProps } from './types';
 
-class UploadPhoto extends Component<IProps> {
-  render() {
-    const { pictureRef, textError, name, handleChangeInput } = this.props;
+const UploadPhoto = ({ pictureRef, textError, name, handleChangeInput }: IProps) => {
+  return (
+    <div className="uploadphoto">
+      <input
+        ref={pictureRef}
+        className="uploadphoto__input"
+        type="file"
+        onChange={() => handleChangeInput(`${name}Error`, textError)}
+        data-testid="uploadphoto"
+      />
 
-    return (
-      <div className="uploadphoto">
-        <input
-          ref={pictureRef}
-          className="uploadphoto__input"
-          type="file"
-          onChange={() => handleChangeInput(`${name}Error`, textError)}
-          data-testid="uploadphoto"
-        />
-
-        {textError !== '' && <div className="uploadphoto__validation">{textError}</div>}
-      </div>
-    );
-  }
-}
+      {textError !== '' && <div className="uploadphoto__validation">{textError}</div>}
+    </div>
+  );
+};
 
 export default UploadPhoto;
