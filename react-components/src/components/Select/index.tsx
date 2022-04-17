72d4901733567c6e1986939085be431bfb3a53d4
sashtje <sashtje.lefevre@gmail.com>
2022-04-17 17:09:14 +0300

    feat: move Select to functional component

--- react-components/src/components/Select/index.tsx
+++ react-components/src/components/Select/index.tsx
@@ -1,36 +1,30 @@
-import { Component } from 'react';
-
 import Label from '../Label';
 
 import './styles.scss';
 
 import { IProps } from './types';
 
-class Select extends Component<IProps> {
-  render() {
-    const { label, selectRef, options, textError, name, handleChangeInput } = this.props;
+const Select = ({ label, selectRef, options, textError, name, handleChangeInput }: IProps) => {
+  return (
+    <div className="selectfield">
+      <Label>
+        {label}
+        <select
+          className="selectfield__select"
+          defaultValue=""
+          ref={selectRef}
+          onChange={() => handleChangeInput(`${name}Error`, textError)}
+        >
+          <option key={-1} disabled></option>
+          {options.map((option, index) => (
+            <option key={index}>{option}</option>
+          ))}
+        </select>
+      </Label>
 
-    return (
-      <div className="selectfield">
-        <Label>
-          {label}
-          <select
-            className="selectfield__select"
-            defaultValue=""
-            ref={selectRef}
-            onChange={() => handleChangeInput(`${name}Error`, textError)}
-          >
-            <option key={-1} disabled></option>
-            {options.map((option, index) => (
-              <option key={index}>{option}</option>
-            ))}
-          </select>
-        </Label>
-
-        {textError !== '' && <div className="selectfield__validation">{textError}</div>}
-      </div>
-    );
-  }
-}
+      {textError !== '' && <div className="selectfield__validation">{textError}</div>}
+    </div>
+  );
+};
 
 export default Select;
