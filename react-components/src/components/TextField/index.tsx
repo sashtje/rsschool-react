29a9fdc7bfa12728dd41a71d81308efec4fb729c
sashtje <sashtje.lefevre@gmail.com>
2022-04-17 17:17:43 +0300

    feat: move TextField to functional component

--- react-components/src/components/TextField/index.tsx
+++ react-components/src/components/TextField/index.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 
 import Label from '../Label';
 
@@ -6,34 +6,28 @@ import './styles.scss';
 
 import { IProps } from './types';
 
-class TextField extends Component<IProps> {
-  componentDidMount() {
-    const { autofocus, inputRef } = this.props;
-
+const TextField = ({ label, inputRef, textError, name, handleChangeInput, autofocus }: IProps) => {
+  useEffect(() => {
     if (autofocus) {
       inputRef.current!.focus();
     }
-  }
+  }, []);
 
-  render() {
-    const { label, inputRef, textError, name, handleChangeInput } = this.props;
+  return (
+    <div className="textfield">
+      <Label>
+        {label}
+        <input
+          ref={inputRef}
+          className="textfield__input"
+          type="text"
+          onChange={() => handleChangeInput(`${name}Error`, textError)}
+        />
+      </Label>
 
-    return (
-      <div className="textfield">
-        <Label>
-          {label}
-          <input
-            ref={inputRef}
-            className="textfield__input"
-            type="text"
-            onChange={() => handleChangeInput(`${name}Error`, textError)}
-          />
-        </Label>
-
-        {textError !== '' && <div className="textfield__validation">{textError}</div>}
-      </div>
-    );
-  }
-}
+      {textError !== '' && <div className="textfield__validation">{textError}</div>}
+    </div>
+  );
+};
 
 export default TextField;
