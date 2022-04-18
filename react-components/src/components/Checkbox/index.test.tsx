e140493a7dfeb8404767b3ac6d62b6fc7ac14a60
sashtje <sashtje.lefevre@gmail.com>
2022-04-18 17:39:19 +0300

    fix: fix tests for Checkbox

--- react-components/src/components/Checkbox/index.test.tsx
+++ react-components/src/components/Checkbox/index.test.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
 import { screen, render } from '@testing-library/react';
 
 import Checkbox from '.';
 
 test('Check render of Checkbox component', () => {
-  /*   const label = 'Test';
-  const checkRef: React.RefObject<HTMLInputElement> = React.createRef();
-  render(<Checkbox label={label} checkboxRef={checkRef} />);
+  const label = 'Test';
+  const name = 'news';
+  const register = jest.fn();
+
+  render(<Checkbox label={label} register={register} name={name} />);
 
   const checkbox = screen.getByRole('checkbox', {
     name: label,
   });
 
-  expect(checkbox).toBeInTheDocument(); */
+  expect(checkbox).toBeInTheDocument();
 });
