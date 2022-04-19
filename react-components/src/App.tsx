cd0483805499d5b4aca10b2b5a3488c6cb126c87
sashtje <sashtje.lefevre@gmail.com>
2022-04-19 16:37:52 +0300

    refactor: move Routes to AppRouter component

--- react-components/src/App.tsx
+++ react-components/src/App.tsx
@@ -1,26 +1,9 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-
-import Main from './pages/Main';
-import Layout from './components/Layout';
+import AppRouter from './components/AppRouter';
 
 import './App.scss';
 
-import { routes } from './router';
-
 const App = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Main />} />
-
-        {routes.map((route, index) => (
-          <Route key={index} path={route.path} element={<route.element />} />
-        ))}
-
-        <Route path="*" element={<Navigate to="/404" replace />} />
-      </Route>
-    </Routes>
-  );
+  return <AppRouter />;
 };
 
 export default App;
