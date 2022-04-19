16e0e744a88fd5050a84c998f5347814d2f4bd86
sashtje <sashtje.lefevre@gmail.com>
2022-04-19 17:40:06 +0300

    refactor: change concatination

--- react-components/src/components/CardList/index.tsx
+++ react-components/src/components/CardList/index.tsx
@@ -6,7 +6,7 @@ import IProps from './types';
 
 const CardList = ({ className, data }: IProps) => {
   return (
-    <div className={className ? className + ' cards' : 'cards'} data-testid="cards">
+    <div className={className ? `${className} cards` : 'cards'} data-testid="cards">
       {data.map((item) => (
         <CardItem card={item} key={item.id} />
       ))}
